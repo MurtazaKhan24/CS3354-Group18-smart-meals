928bc0e686c718bac131832c84e7d2954781a9af
MurtazaKhan24 <khanmurtaza001@gmail.com>
2024-10-15 13:16:44 -0500

    added CSS

---
+++ smart-meals/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Specifies where to look for Tailwind class names
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
